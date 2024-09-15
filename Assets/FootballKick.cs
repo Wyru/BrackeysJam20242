0515fc7d114d875d6799d39f7c8fa499d79cc59e
Andrew Simmons <andrewsimmons007@hotmail.com>
2024-09-15 09:43:14 -0300

    Update FootballKick.cs

--- Assets/FootballKick.cs
+++ Assets/FootballKick.cs
@@ -7,7 +7,7 @@ public class FootballKick : MonoBehaviour
 
     public Rigidbody rb;
     private void OnCollisionEnter(Collision other) {
-        if(other.gameObject.name == "PlayerObj"){
+        if(other.gameObject.tag == "Player"){
             rb.AddForce((other.transform.forward+Vector3.up)*5, ForceMode.Impulse);
         }
     }
